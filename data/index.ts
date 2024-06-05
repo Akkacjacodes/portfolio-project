51a1ce9fcbf914ea27119078f5846bbc31373ddf
Adriana Sobota <universe7raindrop@gmail.com>
2024-06-05 19:00:29 +0200

    work experience update

--- data/index.ts
+++ data/index.ts
@@ -182,22 +182,22 @@ export const companies = [
 export const workExperience = [
   {
     id: 1,
-    title: "Frontend Engineer Intern",
-    desc: "Assisted in the development of a web-based platform using React.js, enhancing interactivity.",
+    title: "Website development",
+    desc: "Independently developed a web-based platform using React.js, enhancing business functionalities.",
     className: "md:col-span-2",
     thumbnail: "/exp1.svg",
   },
   {
     id: 2,
-    title: "Mobile App Dev - JSM Tech",
-    desc: "Designed and developed mobile app for both iOS & Android platforms using React Native.",
+    title: "Next JS development",
+    desc: "Designed and developed banking platform with multiple third party software integrations",
     className: "md:col-span-2", // change to md:col-span-2
     thumbnail: "/exp2.svg",
   },
   {
     id: 3,
-    title: "Freelance App Dev Project",
-    desc: "Led the dev of a mobile app for a client, from initial concept to deployment on app stores.",
+    title: "Dashboard integration",
+    desc: "Connected the companie's pipedrive to independently developed dashboard saving 25% of employee time",
     className: "md:col-span-2", // change to md:col-span-2
     thumbnail: "/exp3.svg",
   },
